4f175cc173a969ac58911b491771149f607ba0af
RendyDermawan10119030 <rendy.10119030@mahasiswa.unikom.ac.id>
2020-10-20 22:46:14 +0700

    First commit

---
+++ src/if1/pkg10119030/latihan7/integer/IF110119030Latihan7Integer.java
@@ -0,0 +1,25 @@
+package if1.pkg10119030.latihan7.integer;
+/**
+ * Nama     : Rendy Dermawan
+ * Kelas    : IF-1
+ * NIM      : 10119030
+ * Deskripsi Program :Latihan Integer
+ */
+public class IF110119030Latihan7Integer {
+   
+    public static void main(String[] args) {
+    //variable Lokal    
+    byte b;
+    short s;
+    int i;
+    long l;
+    b = 120;
+    s = 32767;
+    i = 1_000_000_000;
+    l = 90000000000L;
+    System.out.println("byte : " + b);
+    System.out.println("short : " + s);
+    System.out.println("int : " + i);
+    System.out.println("long : " + l);
+    }   
+}
